c3ef75af824032b23866e5259323c365190ce88d
Shivika Sharma <91414321+shivikasharmaaa@users.noreply.github.com>
2023-11-28 10:36:42 +0530

    Updated sample_main.py

--- sample_main.py
+++ sample_main.py
@@ -1,12 +1,24 @@
 
 from my_model import custom_model
+answer_tokens = ['positive', 'negative']
+c_tokens = [['positive','happy'],['negative','sad']]
 
-c_tokens = [['favor','favoring','preferred'],['against','opposing'],['none','neutral','unbiased']]
-model, tokenizer = custom_model('bert-base-uncased')
+model_no_map, tokenizer = custom_model('bert-base-uncased',answer_tokens)
+model_map, tokenizer = custom_model('bert-base-uncased',answer_tokens, c_tokens)
 
-sentence = "The tweet 'I hate Biden' has stance [MASK] towards Biden."
+sentence = "The text 'I am very happy today' has [MASK] sentiment."
 inputs = tokenizer(sentence, return_tensors='pt', max_length=512, truncation=True, padding='max_length')
 
-print(model(inputs, 1))
-print(model.return_probs(inputs))
-print(model.return_prediction(inputs))
+print("-----------------------------")
+print("Output of model with simple answer tokens")
+print(f"\tOutput of forward pass: {model_no_map(inputs, 1)}")
+print(f"\tClass Token Probabilities: {model_no_map.return_probs(inputs)}")
+print(f"\tPredicted Class Token: {model_no_map.return_prediction(inputs)}")
+
+print("-----------------------------\n")
+print("-----------------------------")
+print("Output of model with mapped answer token set")
+print(f"\tOutput of forward pass: {model_map(inputs, 1)}")
+print(f"\tClass Token Probabilities: {model_map.return_probs(inputs)}")
+print(f"\tPredicted Class Token: {model_map.return_prediction(inputs)}")
+print("-----------------------------")
